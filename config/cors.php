125e26773d6c1a224f74e7cd18949f7da0b0ee17
Michael Asencio <164639782+Michael383883@users.noreply.github.com>
2024-12-11 22:09:01 -0400

    Ajustar configuración CORS

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*'], // Define las rutas donde aplicar CORS, por ejemplo, todos los endpoints en 'api'
     'allowed_methods' => ['*'], // Permite todos los métodos HTTP
-    'allowed_origins' => ['http://localhost:3000'], // Reemplaza con la URL de tu app Next.js
+    'allowed_origins' => ['*'], // Reemplaza con la URL de tu app Next.js
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'], // Permite todos los headers
     'exposed_headers' => [],
